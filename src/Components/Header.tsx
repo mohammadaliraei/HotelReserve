c9818a1815b2d53a94a209488f2483bc57dc8209
mohammadaliraei <mobham.des@gmail.com>
2024-01-21 19:27:01 +0330

    update

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,65 +10,14 @@ import {
   faPlaneUp,
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
-import Nav from "./Nav";
 import SearchBox from "../Components/SearchBox/SearchBox";
+import Nav from "./Nav";
 
 const Header = () => {
-  const [isVisible, setIsVisible] = useState<Boolean>(false);
-  const handelHumburger = () => {
-    setIsVisible(!isVisible);
-  };
-  const Links = [
-    { name: "صفحه اصلی", link: "/", logo: faHouse },
-    { name: "بیمه مسافرتی", link: "/", logo: faNoteSticky },
-    { name: "سفرهای من", link: "/", logo: faPlaneUp },
-    { name: "درباره ما", link: "/", logo: faUsers },
-    { name: "پشتیبانی  021_4045 ", link: "/", logo: faPhone },
-  ];
+  
   return (
-    <div className="">
-      <div className="flex md:flex-row md:px-[108px] justify-between items-center gap-4 h-[104px] w-full bg-white ">
-        <div className="flex items-center ">
-          <button className="font-Mikhak flex items-center gap-2 text-white rounded-md px-4 py-2 bg-Blue">
-            ورود / ثبت نام
-            <FontAwesomeIcon icon={faUser} />
-          </button>
-        </div>
-        <div
-          className="flex flex-col md:flex-row gap-6 font-Mikhak text-lg"
-          dir="rtl"
-        >
-          {Links.map((link) => (
-            <div className="flex flex-col md:flex-row">
-              <div
-                key={link.link}
-                className="flex "
-              >
-                <FontAwesomeIcon
-                  icon={link.logo}
-                  className="md:hidden"
-                />
-                {link.name}
-              </div>
-            </div>
-          ))}
-        </div>
-        <div>
-          <img
-            src={Images.logo}
-            className="w-[148px] h-[48px] text-center "
-          />
-        </div>
-        <button
-          className="md:hidden"
-          onClick={() => handelHumburger()}
-        >
-          <FontAwesomeIcon
-            icon={faBars}
-            className=" w-[24px] h-[24px]"
-          />
-        </button>
-      </div>
+    <div className="flex flex-col w-screen">
+      <Nav />
       <div className="flex">
         <div className="flex relative">
           <img
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
-import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { Images } from "./Images";
+import {
+  faHouse,
+  faNoteSticky,
+  faPlaneUp,
+  faUsers,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 
 const Nav = () => {
-  const [toggle, setToggle] = useState(true);
+  const [isVisible, setIsVisible] = useState<Boolean>(false);
+  const handelHumburger = () => {
+    setIsVisible(!isVisible);
+  };
+  const Names = [
+    { id: 1, name: "صفحه اصلی", link: "/", logo: faHouse },
+    { id: 2, name: "بیمه مسافرتی", link: "/", logo: faNoteSticky },
+    { id: 3, name: "سفرهای من", link: "/", logo: faPlaneUp },
+    { id: 4, name: "درباره ما", link: "/", logo: faUsers },
+    { id: 5, name: "پشتیبانی  021_4045 ", link: "/", logo: faPhone },
+  ];
   return (
-    <div className="flex flex-row gap-6 font-Mikhak">
-      <button
-        className="flex flex-row items-center gap-2"
-        onClick={() => setToggle(!toggle)}
-      >
-        {toggle ? (
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            className="w-4 h-4"
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faChevronUp}
-            className="w-4 h-4"
-          />
-        )}
-        سایر موارد
-      </button>
-      <button>سفرهای من</button>
-      <button>بیمه مسافرتی</button>
-      <button>صفحه اصلی</button>
+    <div
+      className="flex w-full py-4"
+      dir="rtl"
+    >
+      <div className="flex w-full px-14 items-center justify-between">
+        <div>
+          <img src={Images.logo} />
+        </div>
+        <div className="md:flex md:block hidden gap-6 font-bold">
+          {Names.map((name) => (
+            <button
+              key={name.id}
+              className="hover:text-Blue hover:border-Blue text-gray-800 font-Mikhak border-gray-400 border-b-2 py-2"
+            >
+              {name.name}
+            </button>
+          ))}
+        </div>
+        <div
+          dir="rtl"
+          className="md:flex hidden"
+        >
+          <button className="flex flex-row gap-3 text-center items-center bg-Blue text-white font-Mikhak py-2 px-4 rounded-md">
+            <FontAwesomeIcon icon={faUser} />
+            <h1>ورود / ثبت نام</h1>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
